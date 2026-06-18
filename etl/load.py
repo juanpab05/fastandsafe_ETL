79115586eeeb5186e32b0358f31e00ef71fb30d1
juanpab05 <165406198+juanpab05@users.noreply.github.com>
2026-06-18 09:28:26 -0500

    Revert "Modificar columnas de sede"

--- etl/load.py
+++ etl/load.py
@@ -5,5 +5,3 @@ from sqlalchemy import text
 from sqlalchemy.dialects.postgresql import insert
 
 #Aqui va el codigo para cargar las tablas transformadas a la bodega de datos
-def load(table: DataFrame, olap_conn: Engine, tname):
-    table.to_sql(f'{tname}', olap_conn, if_exists='append', index=False)
--- etl/transform.py
+++ etl/transform.py
@@ -10,28 +10,3 @@ from mlxtend.preprocessing import TransactionEncoder
 from pandas import DataFrame
 
 #Aqui va el codigo para transformar las tablas de la base de datos en las dimensiones y hechos que pusimos
-
-def transform_sede(dim_sede: DataFrame) -> DataFrame:
-    """
-    Parametros: dim_sede.
-    Se conservan las columnas requeridas para la dimensión (sede_id, nombre, cliente_id) y se renombran.
-    """
-    dim_sede.drop(columns=dim_sede.columns.difference(["sede_id", "nombre", "cliente_id"]), inplace=True)
-    dim_sede.replace({' ': 'No aplica', '':'No_aplica'}, inplace=True)
-    dim_sede.rename(columns={"sede_id": "key_dim_sede", "nombre": "nombre_sede", "cliente_id": "cliente_id_sede"}, inplace=True)
-    dim_sede.sort_values(by=["key_dim_sede"], inplace=True)
-    dim_sede["saved"] = date.today()
-    return dim_sede
-
-def transform_mensajero(dim_mensajero: DataFrame, tabla_usuario: DataFrame) -> DataFrame:
-    """
-    Parametros: dim_mensajero y tabla_usuario. 
-    Solo se necesita tabla_usuario para extraer el username al que corresponde la "user_id" de cada mensajero.
-    """
-    dim_mensajero = dim_mensajero.join(tabla_usuario.set_index("id"), on="user_id")
-    dim_mensajero.drop(columns=dim_mensajero.columns.difference(["id", "username"]), inplace=True)
-    dim_mensajero.replace({np.nan: 'No aplica', ' ': 'No aplica', '': 'No_aplica'}, inplace=True)
-    dim_mensajero.rename(columns={"id": "key_dim_mensajero", "username": "username_mensajero"}, inplace=True)
-    dim_mensajero.sort_values(by=["key_dim_mensajero"], inplace=True)
-    dim_mensajero["saved"] = date.today()
-    return dim_mensajero
